5f7ab6f1b687ee25ccb40b08b8550342fe36cde1
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-04 20:01:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 }
 
 function deleteFromDriverByKey(driver, key){
-  const newdriver = {...driver};
+  const newdriver = { driver };
   delete newdriver[key];
 }
 
